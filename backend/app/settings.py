bf764501bb6e418ee0144297bd34fe8e49203a80
Said ZAIER <said@MacBook-Said.local>
2025-10-22 22:16:19 +0200

    config: DRF + SimpleJWT + CORS

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -60,10 +60,22 @@ REST_FRAMEWORK = {
   "DEFAULT_AUTHENTICATION_CLASSES": (
     "rest_framework_simplejwt.authentication.JWTAuthentication",
   ),
+  "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],# Updated for JWT authentication
 } # Added for JWT authentication
 
 CORS_ALLOW_ALL_ORIGINS = True # Added for CORS handling
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
 ROOT_URLCONF = 'app.urls'
 
